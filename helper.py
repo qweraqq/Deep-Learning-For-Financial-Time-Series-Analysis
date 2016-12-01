ca744f4865d30c8169044d60d633c0ec3658b195
qweraqq <shenxiangxiang@gmail.com>
2016-12-01 22:13:45 +0800

    first commit

---
+++ helper.py
@@ -0,0 +1,97 @@
+# -*- coding: UTF-8 -*-
+from __future__ import division
+__author__ = 'shenxiangxiang@gmail.com'
+
+import pandas as pd
+import numpy as np
+
+def time_series_normalization(X, mode=0):
+    """
+    Do your own normalization
+    :param X: (nb_examples, nb_features) matrix
+              here, nb_samples a.k.a timesteps
+              X[: ,0] = (open-last_day_close)/last_day_close
+              X[: ,1] = (high-last_day_close)/last_day_close
+              X[: ,2] = (low-last_day_close)/last_day_close
+              X[: ,3] = (close-last_day_close)/last_day_close
+              X[: ,4] = turnover rate or volume if index(mode1)
+              X[: ,5] = close (not included in return value)
+    :param mode: is it index or stock, mode0-stock, mode1-index
+    :return: normalized X
+    """
+    if mode == 1:
+        X[:, 4] = X[:, 4] * 1
+        X[:, 4] = X[:, 4] / 250000000  # shanghai stock market total value
+    else:
+        X[:, 4] /= 100
+    X[:, 3] = X[:, 3]/100  # p_change
+    r_y = np.copy(X[2:, 3])*100
+    tmp = X[1:, :]  # close, remove first day which do not have a previous close
+    last_day_close = X[0:-1, 5]
+    last_day_close = last_day_close.reshape((len(last_day_close), 1))
+    tmp[:, 0:3] = (tmp[:, 0:3] - last_day_close) / last_day_close
+    r_value = np.copy(tmp[:, 0:5])
+    # Now r_value is as described
+    # Do your own normalization
+    # r_value *= 10
+    r_value = np.sign(r_value)*np.log(1+np.abs(r_value*100))
+    return r_value, r_y
+
+
+## very important
+def y_t(y):
+    for idx,_ in enumerate(y):
+        y[idx] = y_transform(y[idx])
+    return y
+
+def y_transform(y):
+    """
+    transform y(p_change) into labels
+    :param y:
+    :return:
+    """
+    if y <= -6:
+        r = 0
+    elif y > -6 and y <= -3:
+        r = 1
+    elif y > -3 and y <= -1:
+        r = 2
+    elif y > -1 and y <= 0:
+        r = 3
+    elif y > 0 and y <= 1:
+        r = 4
+    elif y > 1 and y <= 3:
+         r = 5
+    elif y > 3 and y <= 6:
+        r = 6
+    else:
+        r = 7
+    return r
+
+def read_single_financial_series_file(filename, mode=0):
+    """
+    :param filename: saved stock file, like '600000.csv'
+    :param mode: mode 0 - regular stocks
+                 mode 1 - index
+    :return: tuple (X ,y)
+             X = (1,nb_samples,nb_features) 3d tensor
+             y = (1,nb_samples,1) 3d tensor
+             here, nb_samples a.k.a timesteps
+    """
+    X = None
+    if mode == 0:
+        df = pd.read_csv(filename, sep=',', header=0, usecols=[1, 2, 4, 7, 14, 3])
+        X = df.as_matrix(['open', 'high', 'low', 'p_change', 'turnover', 'close'])
+
+    if mode == 1:
+        df = pd.read_csv(filename, sep=',', header=0, usecols=[1, 2, 4, 5, 7, 3])
+        X = df.as_matrix(['open', 'high', 'low', 'p_change', 'volume', 'close'])
+
+    X = X[::-1, :]
+    X,r_y = time_series_normalization(X, mode=mode)
+    r_X = X[np.newaxis, 0:-1, :]
+    ###### y value transformation
+    # r_y  = y_t(r_y)
+    r_y = r_y.reshape((len(r_y), 1))
+    r_y = r_y[np.newaxis, :, :]
+    return r_X, r_y
---
+++ model.py
@@ -0,0 +1,126 @@
+# -*- coding: UTF-8 -*-
+from __future__ import division
+__author__ = 'shenxiangxiang@gmail.com'
+import os
+from keras.engine.training import Model
+from keras.layers import Input,LSTM, TimeDistributedDense
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from keras.callbacks import EarlyStopping
+from keras.preprocessing import sequence
+from helper import *
+import numpy as np
+
+nb_hidden_units = 200
+dropout = 0.1
+l2_norm_alpha = 0.00001
+
+
+class FinancialTimeSeriesAnalysisModel(object):
+    model = None
+    def __init__(self, nb_time_step, dim_data, batch_size=1, model_path=None):
+        self.model_path = model_path
+        self.model_path = model_path
+        self.batch_size = batch_size
+        self.size_of_input_data_dim = dim_data
+        self.size_of_input_timesteps = nb_time_step
+        self.build()
+        self.weight_loaded = False
+        if model_path is not None:
+            self.load_weights()
+
+    def build(self):
+        dim_data = self.size_of_input_data_dim
+        nb_time_step = self.size_of_input_timesteps
+        financial_time_series_input = Input(shape=(nb_time_step, dim_data))
+
+        lstm_layer_1 = LSTM(output_dim=nb_hidden_units, dropout_U=dropout, dropout_W=dropout, inner_activation='sigmoid',
+                            W_regularizer=l2(l2_norm_alpha), b_regularizer=l2(l2_norm_alpha), activation='tanh',
+                            return_sequences=True)
+        lstm_layer_2 = LSTM(output_dim=nb_hidden_units, dropout_U=dropout, dropout_W=dropout, inner_activation='sigmoid',
+                            W_regularizer=l2(l2_norm_alpha), b_regularizer=l2(l2_norm_alpha), activation='tanh',
+                            return_sequences=True)
+
+        h1 = lstm_layer_1(financial_time_series_input)
+        h2 = lstm_layer_2(h1)
+        time_series_predictions = TimeDistributedDense(1)(h2)
+        self.model = Model(financial_time_series_input, time_series_predictions,
+                           name="deep rnn for financial time series forecasting")
+
+    def reset(self):
+        for l in self.model.layers:
+            if type(l) is LSTM:
+                l.reset_status()
+
+    def compile_model(self, lr=0.0001, arg_weight=1.):
+        optimizer = Adam(lr=lr)
+        loss = 'mse'
+        self.model.compile(optimizer=optimizer, loss=loss)
+
+    def fit_model(self, X, y, X_val = None, y_val = None, epoch=300):
+        early_stopping = EarlyStopping(monitor='val_loss',patience=3, verbose=0)
+        if X_val is None:
+            self.model.fit(X, y, batch_size=self.batch_size, nb_epoch=epoch, validation_split=0.2,
+                           shuffle=True, callbacks=[early_stopping])
+        else:
+            self.model.fit(X, y, batch_size=self.batch_size, nb_epoch=epoch, validation_data=(X_val, y_val),
+                           shuffle=True, callbacks=[early_stopping])
+
+
+    def save(self):
+        self.model.save_weights(self.model_path, overwrite=True)
+
+    def load_weights(self):
+        if os.path.exists(self.model_path):
+            self.model.load_weights(self.model_path)
+            self.weight_loaded = True
+
+    def print_weights(self, weights=None, detail=False):
+        weights = weights or self.model.get_weights()
+        for w in weights:
+            print("w%s: sum(w)=%s, ave(w)=%s" % (w.shape, np.sum(w), np.average(w)))
+        if detail:
+            for w in weights:
+                print("%s: %s" % (w.shape, w))
+
+    def model_eval(self, X, y):
+        y_hat = self.model.predict(X, batch_size=1)
+        count_true = 0
+        count_all = y.shape[1]
+        for i in range(y.shape[1]):
+            count_true = count_true + 1 if y[0,i,0]*y_hat[0,i,0]>0 else count_true
+            print y[0,i,0],y_hat[0,i,0]
+        print count_all,count_true
+
+if __name__ == '__main__':
+    max_len = 200
+    this_file_path = os.path.dirname(os.path.abspath(__file__))
+    training_set_path = os.path.join(this_file_path,"data",)
+    file_list = os.listdir(training_set_path,)
+    X = None
+    y = None
+    for idx, f in enumerate(file_list):
+        file_path = os.path.join(training_set_path, f)
+        X_tmp, y_tmp = read_single_financial_series_file(file_path)
+        X_tmp = sequence.pad_sequences(X_tmp, maxlen=max_len, dtype='float32')
+        y_tmp = sequence.pad_sequences(y_tmp, maxlen=max_len, dtype='float32')
+        X = X_tmp if X is None else np.vstack((X, X_tmp))
+        y = y_tmp if y is None else np.vstack((y, y_tmp))
+
+    training_set_path = os.path.join(this_file_path,"testdata",)
+    file_list = os.listdir(training_set_path,)
+    X_test = None
+    y_test = None
+    for idx, f in enumerate(file_list):
+        file_path = os.path.join(training_set_path, f)
+        X_tmp, y_tmp = read_single_financial_series_file(file_path,mode=1)
+        X_tmp = sequence.pad_sequences(X_tmp, maxlen=max_len, dtype='float32')
+        y_tmp = sequence.pad_sequences(y_tmp, maxlen=max_len, dtype='float32')
+        X_test = X_tmp if X_test is None else np.vstack((X_test, X_tmp))
+        y_test = y_tmp if y_test is None else np.vstack((y_test, y_tmp))
+
+    financial_time_series_model = FinancialTimeSeriesAnalysisModel(200,5,batch_size=32,model_path="ta.model.weights")
+    financial_time_series_model.compile_model()
+    # financial_time_series_model.fit_model(X,y)
+    # financial_time_series_model.save()
+    financial_time_series_model.model_eval(X_test,y_test)
